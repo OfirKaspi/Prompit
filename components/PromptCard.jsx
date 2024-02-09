679ebcdc88d5f3180c375d340bda5d5bcd85949b
Ofir Kaspi <kaspiofir@gmail.com>
2024-02-09 19:02:58 +0200

    F/Profile/Page ready beside handling the edit and delete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,8 @@ import { usePathname, useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+    const pathName = usePathname()
+    const { data: session } = useSession()
     const [copied, setCopied] = useState('')
 
     const handleCopy = () => {
@@ -52,7 +54,24 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             >
                 {post.tag}
             </p>
-        </div>
+
+            {session?._id === post.creator._id && pathName === '/profile' && (
+                <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                    <p
+                        className='font-inter text-sm green_gradient cursor-pointer'
+                        onClick={handleEdit}
+                    >
+                        Edit
+                    </p>
+                    <p
+                        className='font-inter text-sm orange_gradient cursor-pointer'
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </p>
+                </div>
+            )}
+        </div >
     )
 }
 
